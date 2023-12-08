45a80c61e8e41519f84acc04f964f3bfdefdf168
Edvardas <e.dainovskis@gmail.com>
2023-12-08 13:19:30 +0200

    Move to Java folder

--- Java/part02-Part02_05.CarryOn/part02-Part02_05.CarryOn/src/main/java/CarryOn.java
+++ Java/part02-Part02_05.CarryOn/part02-Part02_05.CarryOn/src/main/java/CarryOn.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 
 public class CarryOn {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+    publ
         while (true) {
             System.out.println("Shall we carry on?");
             String input = scanner.nextLine();
             if (input.equals("no")) {
                 break;
-            }
+            }lllllllllllllllllllllllll
 
 
         }
