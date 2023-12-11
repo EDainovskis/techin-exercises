ef2e2e1df2605adf359f65fc6042e13a1669d760
Edvardas <e.dainovskis@gmail.com>
2023-12-11 09:58:17 +0200

    word

---
+++ Java/part02-Part02_28.Word/part02-Part02_28.Word/src/main/java/Word.java
@@ -0,0 +1,16 @@
+
+public class Word {
+
+    public static void main(String[] args) {
+        String result = word();
+        System.out.println("The value returned by numberUno is: " + result);
+    }
+
+    public static String word() {
+       return "hello";
+
+    }
+}
+
+
+    
