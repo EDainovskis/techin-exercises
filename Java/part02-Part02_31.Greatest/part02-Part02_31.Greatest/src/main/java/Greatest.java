4ffae8d2f7bbbdf87666acb5948d0246568cd5e3
Edvardas <e.dainovskis@gmail.com>
2023-12-11 11:42:57 +0200

    Corected Greatest

--- Java/part02-Part02_31.Greatest/part02-Part02_31.Greatest/src/main/java/Greatest.java
+++ Java/part02-Part02_31.Greatest/part02-Part02_31.Greatest/src/main/java/Greatest.java
@@ -2,13 +2,14 @@
 public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
-        if (number1 >= number2 && number1 >= number3) {
-            return number1;
-        } else if (number2 >= number1 && number2 >= number3) {
-            return number2;
-        } else {
-            return number3;
-        }
+        int max = number1;
+
+        if (max <= number2)
+            max = number2;
+
+        if (max <= number3)
+            max = number3;
+           return max;
     }
 
     public static void main(String[] args) {
