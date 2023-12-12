829ccc40c529f6891cf1146b0bbbec2b064dd11d
Edvardas <e.dainovskis@gmail.com>
2023-12-12 15:31:32 +0200

    Complete IndexOf

---
+++ Java/part03-Part03_10.IndexOf/part03-Part03_10.IndexOf/src/main/java/IndexOf.java
@@ -0,0 +1,34 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IndexOf {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            numbers.add(input);
+        }
+
+        System.out.print("Search for? ");
+        int searchValue = scanner.nextInt();
+
+        boolean found = false;
+        System.out.print(searchValue + " is at index");
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) == searchValue) {
+                System.out.print(" " + i);
+                found = true;
+            }
+        }
+
+    }
+}
