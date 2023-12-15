f750ec9f521d0848922bb6a649bc6e8c23872746
Edvardas <e.dainovskis@gmail.com>
2023-12-15 12:51:28 +0200

    Complete Room

---
+++ Java/part04-Part04_04.Room/part04-Part04_04.Room/src/main/java/Room.java
@@ -0,0 +1,11 @@
+public class Room {
+    private String code;
+    private int seats;
+
+    public Room(String code, int numberOfSeats) {
+        this.seats = numberOfSeats;
+        this.code = code;
+
+    }
+
+}
