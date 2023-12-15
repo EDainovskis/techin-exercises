ff5e3560185291514352c429d085e84b35a3eb14
Edvardas <e.dainovskis@gmail.com>
2023-12-15 09:53:05 +0200

    Complete Account

---
+++ Java/part04-Part04_01.YourFirstAccount/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,25 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+
+        Account artosAccount = new Account("Arto's account", 100.0);
+
+        System.out.println("Initial state");
+        System.out.println(artosAccount);
+
+        artosAccount.withdrawal(20);
+        System.out.println("The balance of Arto's account is now: " + artosAccount.saldo());
+artosAccount.deposit(20.0);
+
+
+        System.out.println("End state");
+        System.out.println(artosAccount);
+
+
+
+
+        // Do not touch the code in Account.java
+        // Write your program here
+    }
+}
