97d30ec48a49f9bbd522c6a913564927bc4d4d1d
Edvardas <e.dainovskis@gmail.com>
2023-12-15 13:19:39 +0200

    Complete Whistle

---
+++ Java/part04-Part04_05.Whistle/part04-Part04_05.Whistle/src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Whistle duckWhistle = new Whistle("Kvaak");
+        Whistle roosterWhistle = new Whistle("Peef");
+
+        // Using the sound method
+        duckWhistle.sound();
+        roosterWhistle.sound();
+        duckWhistle.sound();
+    }
+}
+
+
