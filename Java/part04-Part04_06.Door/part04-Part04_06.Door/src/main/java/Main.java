95f1fd6a729ff1dfbe2c7688b0febed8c170a7db
Edvardas <e.dainovskis@gmail.com>
2023-12-15 13:36:44 +0200

    Complete Door

---
+++ Java/part04-Part04_06.Door/part04-Part04_06.Door/src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        Door alexander = new Door();
+
+        alexander.knock();
+        alexander.knock();
+
+        // This is an empty main method
+        // You can use it to test your Door class.
+    }
+}
