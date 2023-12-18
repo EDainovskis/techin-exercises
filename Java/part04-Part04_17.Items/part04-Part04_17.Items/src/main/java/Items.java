386b54603e662e6c99359bf25036a5c899922977
Edvardas <e.dainovskis@gmail.com>
2023-12-18 15:59:21 +0200

    Complete Items

---
+++ Java/part04-Part04_17.Items/part04-Part04_17.Items/src/main/java/Items.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Items {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        ArrayList<Item> itemsList = new ArrayList<>();
+
+        while (true) {
+            System.out.print("Name: ");
+            String itemName = scanner.nextLine();
+
+            if (itemName.isEmpty()) {
+                break;
+            }
+
+            Item newItem = new Item(itemName);
+            itemsList.add(newItem);
+        }
+
+
+        for (Item item : itemsList) {
+            System.out.println(item);
+        }
+    }
+}
