c45957818cf7386b0d718d23ab0c4456d341bc5f
Edvardas <e.dainovskis@gmail.com>
2023-12-19 10:38:59 +0200

    Changed TelevisionPrograms

--- Java/part04-Part04_20.Books/part04-Part04_20.Books/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-
-    public static void main(String[] args) {
-        
-        // implement here the program that allows the user to enter 
-        // book information and to examine them        
-    }
-}
