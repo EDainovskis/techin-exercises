084882f842adff668bb2125c4ac025f12c26dcb0
EDainovskis <153265383+EDainovskis@users.noreply.github.com>
2023-12-20 15:47:15 +0200

    Complete Song

---
+++ Java/part05-Part05_12.Song/src/main/java/Song.java
@@ -0,0 +1,31 @@
+public class Song {
+
+    private String artist;
+    private String name;
+    private int durationInSeconds;
+
+    public Song(String artist, String name, int durationInSeconds) {
+        this.artist = artist;
+        this.name = name;
+        this.durationInSeconds = durationInSeconds;
+    }
+
+    @Override
+    public String toString() {
+        return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Song otherSong = (Song) obj;
+        return this.durationInSeconds == otherSong.durationInSeconds
+                && this.artist.equals(otherSong.artist)
+                && this.name.equals(otherSong.name);
+    }
+}
