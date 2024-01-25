2699e4c3b8077657b8f6d80c650693f47bd239ba
EDainovskis <153265383+EDainovskis@users.noreply.github.com>
2024-01-25 10:20:32 +0200

    added false login test to LoginCart

--- Automatinis/logincart/src/main/java/org/example/Main.java
+++ Automatinis/logincart/src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class Main {
 
@@ -21,7 +22,7 @@ public class Main {
     }
 
     @Test
-    void testSearchBox() {
+    void testPositiveLogin() {
         driver.get("https://demo.opencart-extensions.co.uk/");
 
         WebElement cartButton = driver.findElement(By.cssSelector("#top-links > ul > li.dropdown > a > span.hidden-xs.hidden-sm.hidden-md"));
@@ -30,10 +31,8 @@ public class Main {
         WebElement loginButton = driver.findElement(By.cssSelector("#top-links > ul > li.dropdown.open > ul > li:nth-child(2) > a"));
         loginButton.click();
 
-
         WebElement emailInput = driver.findElement(By.cssSelector("#input-email"));
         emailInput.sendKeys("tester@gmail.com");
-
 
         WebElement passwordInput = driver.findElement(By.cssSelector("#input-password"));
         passwordInput.sendKeys("tester");
@@ -41,14 +40,36 @@ public class Main {
         WebElement loginButton2 = driver.findElement(By.cssSelector("#content > div > div:nth-child(2) > div > form > input"));
         loginButton2.click();
 
-
+        assertTrue(true, "Login should be successful");
     }
 
-    // Uncomment the @AfterAll method if you want to close the browser after the test
-//    @AfterAll
-//    static void tearDown() {
-//        if (driver != null) {
-//            driver.quit();
-//        }
-//    }
+    @Test
+    void testNegativeLogin() {
+        driver.get("https://demo.opencart-extensions.co.uk/");
+
+        WebElement cartButton = driver.findElement(By.cssSelector("#top-links > ul > li.dropdown > a > span.hidden-xs.hidden-sm.hidden-md"));
+        cartButton.click();
+
+        WebElement loginButton = driver.findElement(By.cssSelector("#top-links > ul > li.dropdown.open > ul > li:nth-child(2) > a"));
+        loginButton.click();
+
+        WebElement emailInput = driver.findElement(By.cssSelector("#input-email"));
+        emailInput.sendKeys("false@gmail.com");
+
+        WebElement passwordInput = driver.findElement(By.cssSelector("#input-password"));
+        passwordInput.sendKeys("false");
+
+        WebElement loginButton2 = driver.findElement(By.cssSelector("#content > div > div:nth-child(2) > div > form > input"));
+        loginButton2.click();
+
+        assertFalse(false, "Login should be unsuccessful");
+    }
+
+
+    //    @AfterAll
+    //    static void tearDown() {
+    //        if (driver != null) {
+    //            driver.quit();
+    //        }
+    //    }
 }
