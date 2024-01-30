2abfd3795ff377d7066f342939885ef85daaee28
EDainovskis <153265383+EDainovskis@users.noreply.github.com>
2024-01-30 14:42:34 +0200

    Added POM to the ToDoList

--- Automatinis/ToDoList/src/main/java/org/example/ToDoListPage.java
+++ Automatinis/ToDoList/src/main/java/org/example/ToDoListPage.java
@@ -1,61 +1,51 @@
-//import io.github.bonigarcia.wdm.WebDriverManager;
-//import org.junit.jupiter.api.AfterAll;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//public class Main {
-//
-//    private static WebDriver driver;
-//    private static ToDoListPage toDoListPage;
-//
-//    @BeforeAll
-//    static void setUp() {
-//        WebDriverManager.chromedriver().setup();
-//        driver = new ChromeDriver();
-//        driver.manage().window().maximize();
-//        toDoListPage = new ToDoListPage(driver);
-//    }
-//
-//    @Test
-//    void testWebElement() {
-//        driver.get("https://webdriveruniversity.com/To-Do-List/index.html");
-//
-//        toDoListPage.getInputField().sendKeys("new list");
-//        toDoListPage.getInputField().sendKeys(Keys.ENTER);
-//
-//        assertTrue(toDoListPage.isElementPresent(toDoListPage.getAddedItem()));
-//    }
-//
-//    @Test
-//    void checked() {
-//        driver.get("https://webdriveruniversity.com/To-Do-List/index.html");
-//
-//        toDoListPage.markItemAsCompleted();
-//        assertTrue(toDoListPage.isElementPresent(toDoListPage.getCompletedItem()));
-//
-//        // Rest of your test logic
-//    }
-//
-//    @Test
-//    void delete() {
-//        driver.get("https://webdriveruniversity.com/To-Do-List/index.html");
-//
-//        // Assuming you have an item to delete
-//        toDoListPage.deleteItem();
-//        assertFalse(toDoListPage.isElementPresent(toDoListPage.getAddedItem()));
-//
-//        // Rest of your test logic
-//    }
-//
-//    @AfterAll
-//    static void tearDown() {
-//        if (driver != null) {
-//            driver.quit();
-//        }
-//    }
-//}
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.Keys;
+
+public class ToDoListPage {
+    private final WebDriver driver;
+    private final Actions actions;
+
+    public ToDoListPage(WebDriver driver) {
+        this.driver = driver;
+        this.actions = new Actions(driver);
+    }
+
+    public void addTask(String taskName) {
+        WebElement inputField = driver.findElement(By.cssSelector("#container > input[type=text]"));
+        inputField.sendKeys(taskName);
+        inputField.sendKeys(Keys.ENTER);
+    }
+
+    public void markTaskAsCompleted(int taskIndex) {
+        WebElement task = driver.findElement(By.cssSelector("#container > ul > li:nth-child(" + taskIndex + ")"));
+        actions.click(task).build().perform();
+    }
+
+    public void deleteTask(int taskIndex) {
+        WebElement task = driver.findElement(By.cssSelector("#container > ul > li:nth-child(" + taskIndex + ")"));
+        actions.moveToElement(task).perform();
+        WebElement deleteButton = driver.findElement(By.cssSelector("#container > ul > li:nth-child(" + taskIndex + ") > span"));
+        actions.click(deleteButton).build().perform();
+    }
+
+    public boolean isTaskPresent(String taskName) {
+        WebElement task = driver.findElement(By.xpath("//li[contains(text(), '" + taskName + "')]"));
+        return isElementPresent(task);
+    }
+
+    public boolean isTaskCompleted(int taskIndex) {
+        WebElement task = driver.findElement(By.cssSelector("#container > ul > li:nth-child(" + taskIndex + ")"));
+        return task.getAttribute("class").contains("completed");
+    }
+
+    private boolean isElementPresent(WebElement element) {
+        try {
+            return element.isDisplayed();
+        } catch (org.openqa.selenium.NoSuchElementException e) {
+            return false;
+        }
+    }
+}
--- Automatinis/ToDoList/src/main/java/org/example/ToDoListTest.java
+++ Automatinis/ToDoList/src/main/java/org/example/ToDoListTest.java
@@ -1,71 +1,68 @@
-//import io.github.bonigarcia.wdm.WebDriverManager;
-//import org.junit.jupiter.api.AfterAll;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//import org.openqa.selenium.Keys;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.interactions.Actions;
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//public class ToDoListTest {
-//
-//    private static WebDriver driver;
-//    private static Actions actions;
-//    private static ToDoListPage toDoListPage;
-//
-//    @BeforeAll
-//    static void setUp() {
-//        WebDriverManager.chromedriver().setup();
-//        driver = new ChromeDriver();
-//        driver.manage().window().maximize();
-//        actions = new Actions(driver);
-//        toDoListPage = new ToDoListPage(driver);
-//    }
-//
-//    @Test
-//    void testAddItem() {
-//        driver.get("https://webdriveruniversity.com/To-Do-List/index.html");
-//
-//        toDoListPage.getInputField().sendKeys("New Item");
-//        toDoListPage.getInputField().sendKeys(Keys.ENTER);
-//
-//        assertTrue(toDoListPage.getAddedItem().isDisplayed());
-//    }
-//
-//    @Test
-//    void testCompleteItem() {
-//        driver.get("https://webdriveruniversity.com/To-Do-List/index.html");
-//
-//        // Add an item
-//        toDoListPage.getInputField().sendKeys("New Item");
-//        toDoListPage.getInputField().sendKeys(Keys.ENTER);
-//
-//        // Mark the item as completed
-//        toDoListPage.markItemAsCompleted();
-//
-//        assertTrue(toDoListPage.getCompletedItem().isDisplayed());
-//    }
-//
-//    @Test
-//    void testDeleteItem() {
-//        driver.get("https://webdriveruniversity.com/To-Do-List/index.html");
-//
-//        // Add an item
-//        toDoListPage.getInputField().sendKeys("New Item");
-//        toDoListPage.getInputField().sendKeys(Keys.ENTER);
-//
-//        // Delete the item
-//        toDoListPage.deleteItem();
-//
-//        assertFalse(toDoListPage.isElementPresent(toDoListPage.getToDoItem()));
-//    }
-//
-//    @AfterAll
-//    static void tearDown() {
-//        if (driver != null) {
-//            driver.quit();
-//        }
-//    }
-//}
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ToDoListTest {
+    private static WebDriver driver;
+    private static ToDoListPage toDoListPage;
+
+    @BeforeAll
+    static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        toDoListPage = new ToDoListPage(driver);
+    }
+
+    @Test
+    void testAddTask() {
+        driver.get("https://webdriveruniversity.com/To-Do-List/index.html");
+
+
+        toDoListPage.addTask("new list");
+
+
+        assertTrue(toDoListPage.isTaskPresent("new list"));
+    }
+
+    @Test
+    void testMarkTaskAsCompleted() {
+        driver.get("https://webdriveruniversity.com/To-Do-List/index.html");
+
+
+        toDoListPage.addTask("new list");
+
+
+        toDoListPage.markTaskAsCompleted(1);
+
+
+        assertTrue(toDoListPage.isTaskCompleted(1));
+    }
+
+    @Test
+    void testDeleteTask() {
+        driver.get("https://webdriveruniversity.com/To-Do-List/index.html");
+
+
+        toDoListPage.addTask("new list");
+
+
+        toDoListPage.deleteTask(1);
+
+
+        assertTrue(toDoListPage.isTaskPresent("new list"));
+    }
+
+    @AfterAll
+    static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
